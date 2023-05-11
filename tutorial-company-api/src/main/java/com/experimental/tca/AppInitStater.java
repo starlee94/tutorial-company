f356494ae15e16766f509638f1ee9637a5f91b5f
MYH\star.lee <star.lee@hytechc.com>
2023-05-11 17:26:08 +0800

    Initial Commit

---
+++ tutorial-company-api/src/main/java/com/experimental/tca/AppInitStater.java
@@ -0,0 +1,11 @@
+package com.experimental.tca;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class AppInitStater implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
---
+++ tutorial-company-api/src/main/java/com/experimental/tca/TutorialCompanyApiApplication.java
@@ -0,0 +1,13 @@
+package com.experimental.tca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialCompanyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialCompanyApiApplication.class, args);
+	}
+
+}
