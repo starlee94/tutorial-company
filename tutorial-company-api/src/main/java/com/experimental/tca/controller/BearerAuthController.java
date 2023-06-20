be9ab16b23c142ae6c9a24bbccfd0cc331cd33e5
MYH\star.lee <star.lee@hytechc.com>
2023-06-20 09:38:23 +0800

    Swagger updates on schema and token.

---
+++ tutorial-company-api/src/main/java/com/experimental/tca/controller/BearerAuthController.java
@@ -0,0 +1,10 @@
+package com.experimental.tca.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+/**
+ * @author star.lee
+ */
+@SecurityRequirement(name="bearerAuth")
+public interface BearerAuthController {
+}
