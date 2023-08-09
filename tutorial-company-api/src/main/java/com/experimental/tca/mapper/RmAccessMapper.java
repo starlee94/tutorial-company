d5257c62d87fbac0437526c3cbb70c5051a65ef9
MYH\star.lee <star.lee@hytechc.com>
2023-08-09 16:32:59 +0800

    temporary remove rmAccess update query

--- tutorial-company-api/src/main/java/com/experimental/tca/mapper/RmAccessMapper.java
+++ tutorial-company-api/src/main/java/com/experimental/tca/mapper/RmAccessMapper.java
@@ -1,16 +1,15 @@
 package com.experimental.tca.mapper;
 
-import com.experimental.tca.entity.RmAccess;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
+/**
+ * @author star.lee
+ */
 @Mapper
 public interface RmAccessMapper {
 
     @Select("select i_entry_count_daily from rm_access where i_rm_access_id=#{id}")
     int getAccessCountById(int id);
 
-    @Update("update rm_access set i_entry_count_daily=#{i_entry_count_daily} where i_rm_access_id=#{i_rm_access_id}")
-    void updateAccessCount(RmAccess rmAccess);
 }
